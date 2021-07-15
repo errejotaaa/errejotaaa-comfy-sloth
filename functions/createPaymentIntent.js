ec27cce7d0f04220e1c2bfb2d1f619d7bac40359
errejotaaa <ricardojaa.91@gmail.com>
2021-07-15 01:47:11 -0400

    final commit

---
+++ functions/createPaymentIntent.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY);
+
+exports.handler = async function (event, context) {
+  const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
+
+  const calculateOrderAmount = () => {
+    return shipping_fee + total_amount;
+  };
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(),
+      currency: "usd",
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+    };
+  } catch (error) {
+    console.log(error.response);
+  }
+
+  return {
+    statusCode: 200,
+    body: "Create payment Intent",
+  };
+};
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>Our story</h2>
+            <div className="underline"></div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              eligendi magnam eaque, hic debitis maiores et amet id vel saepe
+              minus distinctio beatae placeat animi obcaecati aut delectus quia.
+              Molestias, sapiente qui, ipsam consequatur aliquid expedita sint
+              quis, autem praesentium exercitationem repudiandae aperiam neque?
+              Eos aperiam quia error quod facilis?
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
